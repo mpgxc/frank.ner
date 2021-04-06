a38f886ab3e85f944fed5d583038e99692a378b4
Mateus Garcia <mpgx5.c@gmail.com>
2021-04-06 04:15:26 -0300

    Create interface.py

---
+++ interface.py
@@ -0,0 +1,17 @@
+
+def input_new_phrase(text):
+    
+    """
+        Retorna uma lista contendo o primeiro elemento como as Palavras e o segundo elemento as Tags (Classes das palavras)
+        
+        >> [['Mateus', 'B-PER'], 
+            ['Pinto',  'I-PER'],
+            ['Garcia', 'I-PER']]
+    """
+    
+    x_new_tokens = [word_idx[word] for word in text.split()]
+    
+    pred = model.predict(np.array([x_new_tokens]))
+    pred = np.argmax(pred, axis=-1)[0]
+    
+    return [[word_list[w], tags[pred]] for (w, pred) in zip(range(len(x_new)), pred)]
